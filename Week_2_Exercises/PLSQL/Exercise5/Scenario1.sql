d0f403263b561bb5415b7b86ccd69187475ed9de
sandeep sahoo <102983296+sandeepsahoo07@users.noreply.github.com>
2024-08-08 12:37:37 +0530

    Delete Week_2_Exercises/PLSQL directory

--- Week_2_Exercises/PLSQL/Exercise5/Scenario1.sql
+++
@@ -1,8 +0,0 @@
-set serveroutput on;
-CREATE OR REPLACE TRIGGER UpdateCustomerLastModified
-BEFORE UPDATE ON Customers
-FOR EACH ROW
-BEGIN
-    :NEW.LastModified := SYSDATE;
-END UpdateCustomerLastModified;
-/
